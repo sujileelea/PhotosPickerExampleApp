c71df68d73649535caec985a81be62a80c0fba24
Lea <sujileelea@gmail.com>
2025-08-25 20:12:11 +0900

    initial commit: add PhotosPickerExampleApp Xcode project

---
+++ PhotosPickerExampleApp/PhotosPickerExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosPickerExampleAppApp.swift
+//  PhotosPickerExampleApp
+//
+//  Created by Suji Lee on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotosPickerExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
